a61a0229c632e7f41ffce9ceb53eb3ea51c32dc1
TNTMaster370 <sainttrystynobjects@gmail.com>
2022-08-14 23:49:35 -0600

    Update VideoReader_Primary.py

--- VideoReader_Primary.py
+++ VideoReader_Primary.py
@@ -2,21 +2,19 @@ from FileManager    import File
 from ScriptManager  import Scripter
 
 def primary():
-  # !!! temp trait!
-  x = "game_script_sample.txt" # Will be inputted manually in final ver.
+  # !!! temp trait; This whole sequence is so I can see if the script is being edited correctly.
   
-  address = "address.txt"
-  classA = File(x)
-
-  y = classA.read()
-  classB = Scripter(y)
-
+  x = "game_script_sample.txt"  # A variable that has the name of the file with the example script.
+  # This will have a file collecting class in the final ver.
+  
+  y = File(x).read()
   print(y)
-  for i in range(2):
-    print("")
-
-  z1, z2 = classB.dissector()
+  
+  z1, z2 = Scripter(y).dissector()
   Scripter(z2).polishBody()
-  pass
+  print(z1)
+  print(z2)
+  
+  # !!! /temp trait
 
 primary()
