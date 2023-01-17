6f6273fd5b067ecd982035a08b6f81019b298081
TNTMaster370 <trystynmcote@gmail.com>
2023-01-17 16:49:07 -0700

    Restructured the Scripter class to function on a generator and allow iterating on it.

--- primary.py
+++ primary.py
@@ -1,11 +1,12 @@
 from script_manager import Scripter
 
-def primary():
-  # !!! this is TEMPORARY, so I can determine if the script is being edited correctly.
-  x = Scripter("sample_script.txt")
-  
-  y = x.polishScript()
-  
-  print(y)
 
-primary()
+def primary():
+    xar = Scripter("sample_script.txt")
+
+    for iar in xar:
+        print(iar)
+
+
+if __name__ == "__main__":
+    primary()
--- script_manager.py
+++ script_manager.py
@@ -1,48 +1,21 @@
-def split(array: list, index):
-  new_array = []
-  har = 0
-  brake = False
-
-  while True:
-    try:
-      har = array.index(index)
-    except:
-      har = len(array)
-      brake = True
-
-    new_array.append([])
-    for i in range(har):
-      new_array[-1].append(array[i])
-    del array[:har+1]
-
-    if brake == True:
-      break
-
-  return(new_array)
-
-
 class Scripter:
-  def __init__(self, file: str):
-    '''
-    The class 'Scripter' interacts with a script, devised from a .txt file. It can splice the
-    header off of the script, if applicable. Or it can polish the script into a readable variable.
-    '''
-    ''' with Statement: Reads the file and inputs it to `self.script`. '''
-    with open(file, "r") as read:
-      self.script = read.readlines()
-    
-    ''' for Loop: Clears the '\n' off the end of each line exclusing the last line. '''
-    for iar in range(len(self.script)):  # For each line in the self.script variable...
-      self.script[iar] = self.script[iar][:-1]  # Cut off the '\n' to that line.
+    def __init__(self, file: str):
+        self.lineCurrent = None
+        self.lineNumber = None
+        self._scriptReader = self._read_script(file)
 
+    def __iter__(self):
+        return self
 
-  def polishScript(self):
-    '''
-    Polishes the body of the script into usable chunks. Returns the polished version of the script.
-    '''
-    for iar in range(len(self.script)-1):
-      self.script[iar] = self.script[iar][:-1]
-      
-    self.script = split(self.script, "") 
-    
-    return self.body
+    def __next__(self):
+        try:
+            self.lineCurrent = next(self._scriptReader)
+            self.lineNumber += 1
+            return self.lineCurrent
+        except StopIteration:
+            raise StopIteration
+
+    def _read_script(self, fileName: str):
+        with open(fileName, "r") as fileStore:
+            for line in fileStore:
+                yield line.rstrip()
