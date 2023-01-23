9503680c5768d3de0e4c8f0c3dabf651aedc3df3
TNTMaster370 <trystynmcote@gmail.com>
2023-01-23 15:02:09 -0700

    Created the FrameDraw class. Currently non-functional.

---
+++ class_FrameDraw.py
@@ -0,0 +1,5 @@
+class FrameDraw:
+    def __init__(self):
+        # Currently non-functional. Requires access to the complete timetable
+        # for it to eventually work. This is currently a stand-in.
+        pass
