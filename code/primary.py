61739fe38c84032bb5a61ec5972cc5c3febcc169
TNTMaster370 <trystynmcote@gmail.com>
2023-01-24 15:18:35 -0700

    Created the 'find_path_of_file()' function to correct find the sample script.

--- code/primary.py
+++ code/primary.py
@@ -1,9 +1,17 @@
 from Scripter import Scripter
 from Compiler import Compiler, CompileHEAD, CompileSET
+import os
+
+
+def find_path_of_file(desiredFile_name: str, absoluteTop: str = "C:\\"):
+    for root, dirs, files in os.walk(absoluteTop):
+        for name in files:
+            if name == desiredFile_name:
+                return os.path.abspath(os.path.join(root, name))
 
 
 def primary():
-    xar = Scripter("sample_script.txt")
+    xar = Scripter(find_path_of_file("sample_script.txt"))
     yar = None
     yar_collect = ()
 
