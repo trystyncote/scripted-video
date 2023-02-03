d744489a0b1f3301f9daa9acdf450322813c7daf
TNTMaster370 <trystynmcote@gmail.com>
2023-02-03 15:10:02 -0700

    Moved the creation of the encoder to here.

--- code/File.py
+++ code/File.py
@@ -1,4 +1,6 @@
 import os
+import string
+import random
 
 
 def find_path_of_file(desiredFile_name: str, absoluteTop: str = "C:\\"):
@@ -7,3 +9,16 @@ def find_path_of_file(desiredFile_name: str, absoluteTop: str = "C:\\"):
             if name == desiredFile_name:
                 return os.path.abspath(os.path.join(root, name))
     raise FileNotFoundError
+
+
+def create_encoder():
+    while True:
+        encoder = ''.join(random.choice(string.ascii_uppercase + string.digits,
+                          k=32))
+
+        try:
+            find_path_of_file(encoder)
+        except FileNotFoundError:
+            break
+
+    return encoder
