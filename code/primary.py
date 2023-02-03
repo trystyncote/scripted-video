0477c657d4dd90b8dc8065a0f922e3b5380740f2
TNTMaster370 <trystynmcote@gmail.com>
2023-02-03 15:10:19 -0700

    Changed the calling of the encoder in the primary file.

--- code/primary.py
+++ code/primary.py
@@ -1,4 +1,4 @@
-from File import find_path_of_file
+from File import find_path_of_file, create_encoder
 from Scripter import Scripter
 from Timetable import Timetable
 from FrameDraw import FrameDraw
@@ -8,7 +8,9 @@ def primary():
     scriptVariable_list = {}
     timetable = []
 
-    script = Scripter(find_path_of_file("sample_script.txt"))
+    encoder = create_encoder()
+
+    script = Scripter(find_path_of_file("sample_script.txt"), encoder)
     compile = None
     compile_collect = ()
     timetableClass = None
@@ -31,9 +33,9 @@ def primary():
             else:
                 timetable.append(compile_collect)
 
-    timetableClass = Timetable(timetable, compile.encoder, scriptVariable_list)
+    timetableClass = Timetable(timetable, encoder, scriptVariable_list)
     frameDrawClass = FrameDraw(timetableClass.timetableSorted,
-                               timetableClass.encoder,
+                               encoder,
                                timetableClass.get_object_names(),
                                script.videoTraits["window_width"],
                                script.videoTraits["window_height"])
