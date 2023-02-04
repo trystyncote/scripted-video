809bb80fc234551b59ea66c7a97b6472dca907b5
TNTMaster370 <trystynmcote@gmail.com>
2023-02-03 22:24:07 -0700

    Adjusted primary() to suit changes to the Scripter class.

--- code/primary.py
+++ code/primary.py
@@ -1,17 +1,18 @@
 from File import find_path_of_file, create_encoder
 from Scripter import Scripter
-from Compiler import define_prefix
+from Compiler import define_prefix, CompileSET, CompileOBJECT
 from Timetable import Timetable
 from FrameDraw import FrameDraw
 
 
 def primary():
+    script_variables = {}
     script_video_traits = {}
     timetable = []
 
     encoder = create_encoder()
 
-    script = Scripter(find_path_of_file("sample_script.txt"), encoder)
+    script = Scripter(find_path_of_file("sample_script.txt"))
     compile = None
     compile_collect = ()
     timetableClass = None
@@ -20,7 +21,7 @@ def primary():
         script.clear_comments()
         script.find_line_end()
 
-        if script.linePrevious:
+        if not script.current_line:
             continue
 
         compile = define_prefix(script.current_line, script_video_traits)
@@ -30,6 +31,11 @@ def primary():
             continue
 
         compile_collect = compile.classify_information()
+
+        if len(compile_collect) == 2:
+            script_variables[compile_collect[0]] = compile_collect[1]
+            continue
+
         timetable.append(compile_collect)
 
     timetableClass = Timetable(timetable, encoder, script_video_traits)
