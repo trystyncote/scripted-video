592d068091aecabf149ccffaeba4de26635933c2
TNTMaster370 <trystynmcote@gmail.com>
2023-02-05 00:07:42 -0700

    Added comments and docstrings to the Compile file.

--- code/Compiler.py
+++ code/Compiler.py
@@ -1,8 +1,16 @@
-from File import find_path_of_file
-
-
 def define_prefix(current_line: str, traits: dict):
-    split_current_line = current_line.split(" ")
+    """
+    Collects the information about the respective command from the
+    'current_line' variable.
+
+    :param current_line: The line with the command in it.
+    :param traits: The video's traits, such as frame_rate.
+    :return: Returns the necessary information from the command without the
+        extraneous content.
+    """
+    split_current_line = current_line.split(" ")  # split_current_line houses
+    # the tuple with the information separated by whitespace. This is for
+    # readability purposes.
 
     if split_current_line[0].upper() == "HEAD":
         return tuple(_command_head(current_line))
@@ -28,14 +36,22 @@ def define_prefix(current_line: str, traits: dict):
 
 
 def _discover_syntax(current_line: str, syntax: tuple):
+    # These variables are to prevent a command from taking effect in the middle
+    # of a string, when it's not supposed to.
     outstanding_single_quotes = False
     outstanding_double_quotes = False
 
-    syntax_index = 0
-    line_index = 0
-    line_data = []
+    # The syntax has variable members ("@something") that are near-impossible
+    # to track as-is, so the other members are used as *anchors* to guide the
+    # rest.
+
+    syntax_index = 0  # The purpose of syntax_index is to be a separate counter
+    # to the index in the upcoming for-loop. In particular, syntax_index is
+    # counting indexes of the syntax variable, as opposed to the current_line.
+    line_data = []  # line_data is the list with the commands in them.
 
     for index, contents in enumerate(current_line):
+        # Checks for outstanding single quotes (').
         if not outstanding_single_quotes and contents == "\'":
             outstanding_single_quotes = True
             continue
@@ -44,6 +60,7 @@ def _discover_syntax(current_line: str, syntax: tuple):
                 outstanding_single_quotes = False
             continue
 
+        # Checks for outstanding double quotes (").
         if not outstanding_double_quotes and contents == "\"":
             outstanding_double_quotes = True
             continue
@@ -53,40 +70,53 @@ def _discover_syntax(current_line: str, syntax: tuple):
             continue
 
         if (current_line[index:index+len(syntax[syntax_index])].upper()
-                == syntax[syntax_index]):
+                == syntax[syntax_index]):  # This line is looking for the
+            # current member of the syntax, denoted by syntax_index. This helps
+            # the program find the anchors for the syntax.
             if syntax_index != 0:
+                # If the syntax_index is for the first element, then there is
+                # no previous element, meaning the system doesn't collect it.
                 line_data.append(current_line[syntax_index:index])
             syntax_index = index + len(syntax[syntax_index])
-            line_data.append(current_line[index:index+len(syntax[syntax_index])])
+            line_data.append(current_line[index:index+syntax_index])
 
-            while True:
-                syntax_index += 1
-                try:
-                    if syntax[syntax_index][0] != "@":
-                        break
-                except IndexError:
-                    break
+            # syntax_index needs to update by two to move to the next part of
+            # the syntax that isn't a variable part (@something). No member of
+            # syntax should have two variable parts in a row, or two
+            # non-variable parts in a row. There needs be always a seperator
+            # between variable parts, lest this code breaks..
+            syntax_index += 2
 
             try:
-                syntax[syntax_index]
+                syntax[syntax_index]  # This statement just exists to raise an
+                # IndexError if the syntax_index is out of range.
             except IndexError:
                 line_data.append(current_line[index+len(line_data[-1]):])
+                # The remainder of the current_line is pasted into the
+                # line_data to allow it to exist. This usually takes everything
+                # in the line after the previous anchor.
                 break
 
     return line_data
 
 
 def _manage_time(time_string: str, frame_rate: int):
-    time_string = time_string.split(" ")
+    time_string = (time_string + " ").split(" ")  # time_string splits itself
+    # by the whitespace because the full command must be split that way by
+    # default. Example: "2s 15f" refers to 2 seconds, and 15 frames after that.
     _ = time_string.pop(-1)
+    # f: frame, s: seconds, m: minutes, h: hours
     suffix_effect = {"f": 1,
-                     "s": frame_rate,
-                     "m": frame_rate*60,
-                     "h": frame_rate*60*60}
+                     "s": frame_rate,  # The amount of frames per second is a
+                     "m": frame_rate*60,  # variable amount, so it's manually
+                     "h": frame_rate*60*60}  # calculated here.
 
-    time = 0.0
+    time = 0.0  # This variable is to store the amount of frames per unit as
+    # it is iterated over.
     for i in time_string:
-        time += float(i[:-1]) * suffix_effect[i[-1]]
+        time += float(i[:-1]) * suffix_effect[i[-1]]  # The float member of the
+        # number 'i[:-1]', meaning before the suffix, is multiplied by the
+        # suffix effect denoted by the specific character.
 
     return int(time)
 
@@ -96,10 +126,13 @@ def _command_head(current_line: str):
     keyword_list = ["window_width", "window_height", "frame_rate", "file_name"]
     line_data = _discover_syntax(current_line, syntax)
 
+    # These 'classify' grade variables are based on position, which is based on
+    # syntax.
     classify_variable = line_data[1]
     classify_value = line_data[3]
     try:
-        classify_value = int(classify_value)
+        classify_value = int(classify_value)  # Attempts to convert the value
+        # into an integer, but only if it can. If it can't, it moves on.
     except ValueError:
         pass
 
@@ -115,6 +148,8 @@ def _command_set(current_line: str, **traits):
     syntax = ("SET ", "@variable", "=", "@value", " AS ", "@type")
     line_data = _discover_syntax(current_line, syntax)
 
+    # These 'classify' grade variables are based on position, which is based on
+    # syntax.
     classify_variable = line_data[1]
     classify_value = line_data[3]
     classify_type = line_data[5]
@@ -151,6 +186,8 @@ def _command_object_create(current_line: str, **traits):
               "@layer")
     line_data = _discover_syntax(current_line, syntax)
 
+    # These 'classify' grade variables are based on position, which is based on
+    # syntax.
     classify_object = line_data[1]
     classify_file_name = line_data[3]
     classify_initial_time = _manage_time(line_data[5], traits["frame_rate"])
@@ -159,6 +196,9 @@ def _command_object_create(current_line: str, **traits):
     classify_scale = float(line_data[11])
     classify_layer = int(line_data[13])
 
+    # The "C" is to denote two things: first, that this is a result of the
+    # OBJECT keyword family. Second, that among that family, this is for an
+    # object's creation/initialization.
     return "C", classify_object, classify_file_name, classify_initial_time, \
         classify_x_coord, classify_y_coord, classify_scale, classify_layer
 
@@ -168,6 +208,8 @@ def _command_object_move(current_line: str, **traits):
               "@xchange", ",", "@ychange", ",", "@scale", ",", "@rate")
     line_data = _discover_syntax(current_line, syntax)
 
+    # These 'classify' grade variables are based on position, which is based on
+    # syntax.
     classify_object = line_data[1]
     classify_change_time = _manage_time(line_data[3], traits["frame_rate"])
     classify_x_change = int(line_data[5])
@@ -175,6 +217,9 @@ def _command_object_move(current_line: str, **traits):
     classify_scale_change = float(line_data[9])
     classify_rate = _manage_time(line_data[11], traits["frame_rate"])
 
+    # The "M" is to denote two things: first, that this is a result of the
+    # OBJECT keyword family. Second, that among that family, this is for the
+    # act of moving an object.
     return "M", classify_object, classify_change_time, classify_x_change, \
         classify_y_change, classify_scale_change, classify_rate
 
@@ -184,8 +229,13 @@ def _command_object_delete(current_line: str, **traits):
               "@delay")
     line_data = _discover_syntax(current_line, syntax)
 
+    # These 'classify' grade variables are based on position, which is based on
+    # syntax.
     classify_object = line_data[1]
     classify_delete_time = _manage_time(line_data[3], traits["frame_rate"])
     classify_delay = _manage_time(line_data[5], traits["frame_rate"])
 
+    # The "D" is to denote two things: first, that this is a result of the
+    # OBJECT keyword family. Second, that among that family, this is for an
+    # object's deletion.
     return "D", classify_object, classify_delete_time, classify_delay
