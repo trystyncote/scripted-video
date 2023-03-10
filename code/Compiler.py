69b25032a022f61a906ba41fcccede29f5693e78
TNTMaster370 <trystynmcote@gmail.com>
2023-03-10 11:19:48 -0700

    Removed previous version of syntax collection.

--- code/Compiler.py
+++ code/Compiler.py
@@ -19,121 +19,20 @@ def define_prefix(current_line: str, traits: dict):
 
     if split_current_line[0].upper() == "HEAD":
         return _command_head(current_line, **traits), 1
-        # h = tuple(h)
-        # return h
 
     elif split_current_line[0] == "SET":
         return _command_set(current_line, **traits), 1
 
     elif split_current_line[0] == "CREATE" and split_current_line[1] == "OBJECT":
-        return _command_object_create_regex(current_line, **traits), 2
+        return _command_object_create(current_line, **traits), 2
 
     elif split_current_line[0] == "MOVE" and split_current_line[1] == "OBJECT":
-        return _command_object_move_regex(current_line, **traits), 2
+        return _command_object_move(current_line, **traits), 2
 
     elif split_current_line[0] == "DELETE" and split_current_line[1] == "OBJECT":
-        return _command_object_delete_regex(current_line, **traits), 2
+        return _command_object_delete(current_line, **traits), 2
 
-    '''
-    elif split_current_line[1] == "OBJECT":
-        # if split_current_line[0] == "CREATE":
-        #     return _command_object_create(current_line, **traits)
-
-        # el
-        if split_current_line[0] == "MOVE":
-            return _command_object_move(current_line, **traits)
-
-        elif split_current_line[0] == "DELETE":
-            return _command_object_delete(current_line, **traits)
-
-    # %&$ Raise exception for the script.
-    return None
-    '''
-
-
-def _update_line_data(line_data: list, current_line: str, start_index: int, end_index: int):
-    line_data.append(current_line[start_index:end_index])
-    # line_data[-1] = line_data[-1].split()[0]
-    return line_data
-
-
-def _discover_syntax(current_line: str, syntax: tuple):
-    # These variables are to prevent a command from taking effect in the middle
-    # of a string, when it's not supposed to.
-    outstanding_single_quotes = False
-    outstanding_double_quotes = False
-
-    # The syntax has variable members ("@something") that are near-impossible
-    # to track as-is, so the other members are used as *anchors* to guide the
-    # rest.
-
-    syntax_index = 0  # The purpose of syntax_index is to be a separate counter
-    # to the index in the upcoming for-loop. In particular, syntax_index is
-    # counting indexes of the syntax variable, as opposed to the current_line.
-    line_index = 0
-    line_data = []  # line_data is the list with the commands in them.
-
-    for index, contents in enumerate(current_line):
-        # Checks for outstanding single quotes (').
-        if not outstanding_single_quotes and contents == "\'":
-            outstanding_single_quotes = True
-            continue
-        elif outstanding_single_quotes:
-            if contents == "\'":
-                outstanding_single_quotes = False
-            continue
-
-        # Checks for outstanding double quotes (").
-        if not outstanding_double_quotes and contents == "\"":
-            outstanding_double_quotes = True
-            continue
-        elif outstanding_double_quotes:
-            if contents == "\"":
-                outstanding_double_quotes = False
-            continue
-
-        if (current_line[index:index+len(syntax[syntax_index])].upper()
-                == syntax[syntax_index]):  # This line is looking for the
-            # current member of the syntax, denoted by syntax_index. This helps
-            # the program find the anchors for the syntax.
-            if syntax_index != 0:
-                # If the syntax_index is for the first element, then there is
-                # no previous element, meaning the system doesn't collect it.
-                # line_data.append(current_line[line_index:index])
-                # line_data[-1] = line_data[-1].strip()
-                line_data = _update_line_data(line_data, current_line,
-                                              line_index, index)
-            line_index = index + len(syntax[syntax_index])
-            # line_data.append(current_line[index:index+len(syntax[syntax_index])])
-            # line_data[-1] = line_data[-1].strip()
-            line_data = _update_line_data(line_data, current_line, index,
-                                          index+len(syntax[syntax_index]))
-
-            # syntax_index needs to update by two to move to the next part of
-            # the syntax that isn't a variable part (@something). No member of
-            # syntax should have two variable parts in a row, or two
-            # non-variable parts in a row. There needs be always a seperator
-            # between variable parts, lest this code breaks.
-            syntax_index += 2
-
-            try:
-                syntax[syntax_index]  # This statement just exists to raise an
-                # IndexError if the syntax_index is out of range.
-            except IndexError:
-                # line_data.append(current_line[index+len(line_data[-1]):])
-                # line_data[-1] = line_data[-1].split()
-                line_data = _update_line_data(line_data, current_line,
-                                              index+len(line_data[-1]),
-                                              len(current_line))
-                # The remainder of the current_line is pasted into the
-                # line_data to allow it to exist. This usually takes everything
-                # in the line after the previous anchor.
-                break
-
-    for index, contents in enumerate(line_data):
-        line_data[index] = contents.split()[0]
-
-    return line_data
+    raise UserWarning("Temporary exception for an unrecognized command.")
 
 
 def _manage_time(time_string: str, frame_rate: int):
@@ -249,7 +148,7 @@ def _command_set(current_line: str, **traits):
     return traits
 
 
-def _command_object_create_regex(current_line: str, **traits):
+def _command_object_create(current_line: str, **traits):
     syntax_full = "CREATE OBJECT \w*: \w*"
     # CREATE OBJECT objectname: filename, start_time, ...
     # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^~~~~~~~~~~~~~ ...
@@ -289,7 +188,7 @@ def _command_object_create_regex(current_line: str, **traits):
     return keys_contained
 
 
-def _command_object_move_regex(current_line: str, **traits):
+def _command_object_move(current_line: str, **traits):
     syntax_full = "MOVE OBJECT \w*: \w*"
     # MOVE OBJECT objectname: change_time, x_change, ...
     # ^^^^^^^^^^^^^^^^^^^^^^^~~~~~~~~~~~~~~~~~~~~~~~ ...
@@ -328,7 +227,7 @@ def _command_object_move_regex(current_line: str, **traits):
     return keys_contained
 
 
-def _command_object_delete_regex(current_line: str, **traits):
+def _command_object_delete(current_line: str, **traits):
     syntax_full = "DELETE OBJECT \w*: \w*"
     # DELETE OBJECT objectname: delete_time, ...
     # ^^^^^^^^^^^^^^^^^^^^^^^^^~~~~~~~~~~~~~ ...
@@ -379,65 +278,3 @@ def _split_extra_keys(keys_contained: list, keys_series: list):
         keys_contained[-1][0] = keys_contained[-1][0].lower()
         keys_contained[-1] = tuple(keys_contained[-1])
     return keys_contained
-
-
-def _command_object_create(current_line: str, **traits):
-    # CREATE OBJECT @objectname: @filename, @initialtime, @x, @y, @scale, @layer
-    syntax = ("CREATE OBJECT ", "@objectname", ":", "@filename", ",",
-              "@initialtime", ",", "@x", ",", "@y", ",", "@scale", ",",
-              "@layer")
-    line_data = _discover_syntax(current_line, syntax)
-
-    # These 'classify' grade variables are based on position, which is based on
-    # syntax.
-    classify_object = line_data[1]
-    classify_file_name = line_data[3]
-    classify_initial_time = _manage_time(line_data[5], traits["_HEAD"]["frame_rate"])
-    classify_x_coord = int(line_data[7])
-    classify_y_coord = int(line_data[9])
-    classify_scale = float(line_data[11])
-    classify_layer = int(line_data[13])
-
-    # The "C" is to denote two things: first, that this is a result of the
-    # OBJECT keyword family. Second, that among that family, this is for an
-    # object's creation/initialization.
-    return "C", classify_object, classify_file_name, classify_initial_time, \
-        classify_x_coord, classify_y_coord, classify_scale, classify_layer
-
-
-def _command_object_move(current_line: str, **traits):
-    syntax = ("MOVE OBJECT ", "@objectname", ":", "@changetime", ",",
-              "@xchange", ",", "@ychange", ",", "@scale", ",", "@rate")
-    line_data = _discover_syntax(current_line, syntax)
-
-    # These 'classify' grade variables are based on position, which is based on
-    # syntax.
-    classify_object = line_data[1]
-    classify_change_time = _manage_time(line_data[3], traits["_HEAD"]["frame_rate"])
-    classify_x_change = int(line_data[5])
-    classify_y_change = int(line_data[7])
-    classify_scale_change = float(line_data[9])
-    classify_rate = _manage_time(line_data[11], traits["_HEAD"]["frame_rate"])
-
-    # The "M" is to denote two things: first, that this is a result of the
-    # OBJECT keyword family. Second, that among that family, this is for the
-    # act of moving an object.
-    return "M", classify_object, classify_change_time, classify_x_change, \
-        classify_y_change, classify_scale_change, classify_rate
-
-
-def _command_object_delete(current_line: str, **traits):
-    syntax = ("DELETE OBJECT ", "@objectname", ":", "@deletetime", ",",
-              "@delay")
-    line_data = _discover_syntax(current_line, syntax)
-
-    # These 'classify' grade variables are based on position, which is based on
-    # syntax.
-    classify_object = line_data[1]
-    classify_delete_time = _manage_time(line_data[3], traits["_HEAD"]["frame_rate"])
-    classify_delay = _manage_time(line_data[5], traits["_HEAD"]["frame_rate"])
-
-    # The "D" is to denote two things: first, that this is a result of the
-    # OBJECT keyword family. Second, that among that family, this is for an
-    # object's deletion.
-    return "D", classify_object, classify_delete_time, classify_delay
