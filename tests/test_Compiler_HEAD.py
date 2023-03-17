ec5a5a301e6cdd606f7460c03449a9d141a3ac55
TNTMaster370 <trystynmcote@gmail.com>
2023-03-17 12:57:45 -0600

    Created a test file for the Compiler file. Only manages the HEAD keyword.

---
+++ tests/test_Compiler_HEAD.py
@@ -0,0 +1,48 @@
+from scripted_video.Compiler import define_prefix
+import pytest
+
+
+def return_traits_example():
+    return {
+        "_HEAD": {
+            "_script_name": "...",
+            "file_name": None,
+            "frame_rate": None,
+            "window_width": None,
+            "window_height": None
+        }
+    }
+
+
+@pytest.mark.parametrize("test_input,keyword,expected",
+    [("HEAD window_width = 852", "window_width", 852),
+     ("HEAD window_height = 532", "window_height", 532),
+     ("HEAD frame_rate = 24", "frame_rate", 24),
+     ("HEAD file_name = file_result", "file_name", "file_result")]
+)
+def test_syntax_success(test_input, keyword, expected):
+    result, _ = define_prefix(test_input, return_traits_example())
+    assert result["_HEAD"][keyword] == expected
+
+
+@pytest.mark.parametrize("test_input",
+    ["HEAD windwo_width = 852",
+     "HEAD window_hright = 532",
+     "HEAD frame_rat = 24",
+     "HEAD file_naem = file_result",
+     "HEAD not a keyword = some_value"]
+)
+def test_syntax_failure_keyword(test_input):
+    with pytest.raises(UserWarning):
+        define_prefix(test_input, return_traits_example())
+
+
+@pytest.mark.parametrize("test_input",
+    ["HEAD window_width = ae",
+     "HEAD window_height = ae",
+     "HEAD frame_rate = af",
+     "HEAD file_name = "]
+)
+def test_syntax_failure_value(test_input):
+    with pytest.raises(ValueError):
+        define_prefix(test_input, return_traits_example())
