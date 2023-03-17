3c291c93461165ea8691d8ce47d993e410a86363
TNTMaster370 <trystynmcote@gmail.com>
2023-03-17 12:10:22 -0600

    Changed file searching.

--- src/scripted_video/File.py
+++ src/scripted_video/File.py
@@ -3,12 +3,23 @@ import string
 import random
 
 
-def find_path_of_file(desiredFile_name: str, absoluteTop: str = "C:\\"):
-    for root, dirs, files in os.walk(absoluteTop):
-        for name in files:
-            if name == desiredFile_name:
-                return os.path.abspath(os.path.join(root, name))
-    raise FileNotFoundError
+def find_file(start_dir, filename):
+    for root, dirs, files in os.walk(start_dir):
+        if filename in files:
+            return os.path.join(root, filename)
+    return None
+
+
+def find_path_of_file(filename):
+    current_dir = os.path.abspath('.')
+    while True:
+        file_path = find_file(current_dir, filename)
+        if file_path:
+            return file_path
+        parent_dir = os.path.dirname(current_dir)
+        if parent_dir == current_dir:
+            raise FileNotFoundError
+        current_dir = parent_dir
 
 
 def create_encoder():
