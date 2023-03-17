3d5f7801f6002a14d2bc19b312db69dca3c39f00
TNTMaster370 <trystynmcote@gmail.com>
2023-03-17 13:16:53 -0600

    Added comments.

--- tests/test_Compiler_HEAD.py
+++ tests/test_Compiler_HEAD.py
@@ -21,6 +21,7 @@ def return_traits_example():
      ("HEAD file_name = file_result", "file_name", "file_result")]
 )
 def test_syntax_success(test_input, keyword, expected):
+    # Tests for whether the syntax is successfully being dissected.
     result, _ = define_prefix(test_input, return_traits_example())
     assert result["_HEAD"][keyword] == expected
 
@@ -33,6 +34,8 @@ def test_syntax_success(test_input, keyword, expected):
      "HEAD not a keyword = some_value"]
 )
 def test_syntax_failure_keyword(test_input):
+    # Tests for whether a UserWarning is raised when the keyword is not proper.
+    # This includes typos and keywords that don't exist.
     with pytest.raises(UserWarning):
         define_prefix(test_input, return_traits_example())
 
@@ -44,5 +47,8 @@ def test_syntax_failure_keyword(test_input):
      "HEAD file_name = "]
 )
 def test_syntax_failure_value(test_input):
+    # Tests for whether a UserWarning is raised when the wrong type of value
+    # exists. This includes a string when an integer is expected, or when it
+    # doesn't exist in the first place.
     with pytest.raises(ValueError):
         define_prefix(test_input, return_traits_example())
