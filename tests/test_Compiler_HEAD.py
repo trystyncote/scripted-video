e213e5a027c835274fb19fd464d828588d1f1281
TNTMaster370 <trystynmcote@gmail.com>
2023-04-07 12:56:14 -0600

    Fixed a formatting issue with the 'parametrize' decorator.

--- tests/test_Compiler_HEAD.py
+++ tests/test_Compiler_HEAD.py
@@ -14,25 +14,25 @@ def return_traits_example():
     }
 
 
-@pytest.mark.parametrize("test_input,keyword,expected",
-    [("HEAD window_width = 852", "window_width", 852),
-     ("HEAD window_height = 532", "window_height", 532),
-     ("HEAD frame_rate = 24", "frame_rate", 24),
-     ("HEAD file_name = file_result", "file_name", "file_result")]
-)
+@pytest.mark.parametrize("test_input,keyword,expected", [
+    ("HEAD window_width = 852", "window_width", 852),
+    ("HEAD window_height = 532", "window_height", 532),
+    ("HEAD frame_rate = 24", "frame_rate", 24),
+    ("HEAD file_name = file_result", "file_name", "file_result")
+])
 def test_syntax_success(test_input, keyword, expected):
     # Tests for whether the syntax is successfully being dissected.
     result, _ = define_prefix(test_input, return_traits_example())
     assert result["_HEAD"][keyword] == expected
 
 
-@pytest.mark.parametrize("test_input",
-    ["HEAD windwo_width = 852",
-     "HEAD window_hright = 532",
-     "HEAD frame_rat = 24",
-     "HEAD file_naem = file_result",
-     "HEAD not a keyword = some_value"]
-)
+@pytest.mark.parametrize("test_input", [
+    "HEAD windwo_width = 852",
+    "HEAD window_hright = 532",
+    "HEAD frame_rat = 24",
+    "HEAD file_naem = file_result",
+    "HEAD not a keyword = some_value"
+])
 def test_syntax_failure_keyword(test_input):
     # Tests for whether a UserWarning is raised when the keyword is not proper.
     # This includes typos and keywords that don't exist.
@@ -40,12 +40,12 @@ def test_syntax_failure_keyword(test_input):
         define_prefix(test_input, return_traits_example())
 
 
-@pytest.mark.parametrize("test_input",
-    ["HEAD window_width = ae",
-     "HEAD window_height = ae",
-     "HEAD frame_rate = af",
-     "HEAD file_name = "]
-)
+@pytest.mark.parametrize("test_input", [
+    "HEAD window_width = ae",
+    "HEAD window_height = ae",
+    "HEAD frame_rate = af",
+    "HEAD file_name = "
+])
 def test_syntax_failure_value(test_input):
     # Tests for whether a UserWarning is raised when the wrong type of value
     # exists. This includes a string when an integer is expected, or when it
--- tests/test_Compiler_SET.py
+++ tests/test_Compiler_SET.py
@@ -19,24 +19,24 @@ def return_traits_example():
     }
 
 
-@pytest.mark.parametrize("test_input,keyword,var_type,expected",
-    [("SET hello_world = \"Hello, world!\" AS STRING", "hello_world", "STRING", "\"Hello, world!\""),
-     ("SET addr = __current_address__ AS ADDRESS", "addr", "ADDRESS", "src\\script\\"),
-     ("SET decimal = 0.45 AS FLOAT", "decimal", "FLOAT", 0.45),
-     ("SET not_decimal = 3 AS INT", "not_decimal", "INT", 3),
-     ("SET boolean = False AS BOOL", "boolean", "BOOL", False)]
-)
+@pytest.mark.parametrize("test_input,keyword,var_type,expected", [
+    ("SET hello_world = \"Hello, world!\" AS STRING", "hello_world", "STRING", "\"Hello, world!\""),
+    ("SET addr = __current_address__ AS ADDRESS", "addr", "ADDRESS", "src\\script\\"),
+    ("SET decimal = 0.45 AS FLOAT", "decimal", "FLOAT", 0.45),
+    ("SET not_decimal = 3 AS INT", "not_decimal", "INT", 3),
+    ("SET boolean = False AS BOOL", "boolean", "BOOL", False)
+])
 def test_syntax_success(test_input, keyword, var_type, expected):
     # Tests for whether the syntax is successfully being dissected.
     result, _ = define_prefix(test_input, return_traits_example())
     assert result[var_type][keyword] == expected
 
 
-@pytest.mark.parametrize("test_input",
-    ["SET variable = \"Hello, world!\" AS INT",
-     "SET variable = \"Hello, world!\" AS FlOAT",
-     "SET variable = \"Hello, world!\" AS BOOL"]
-)
+@pytest.mark.parametrize("test_input", [
+    "SET variable = \"Hello, world!\" AS INT",
+    "SET variable = \"Hello, world!\" AS FlOAT",
+    "SET variable = \"Hello, world!\" AS BOOL"
+])
 def test_syntax_failure_value_error(test_input):
     with pytest.raises(ValueError):
         define_prefix(test_input, return_traits_example())
