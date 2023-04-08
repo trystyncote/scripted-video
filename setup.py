cd61e2126a9dfa8959e889b7d49e847918a5dd77
TNTMaster370 <trystynmcote@gmail.com>
2023-04-07 19:20:58 -0600

    Added support for testing virtual environments with tox.

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
-from setuptools import setup
+from setuptools import setup  # , find_packages
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        extras_require={
+            "testing": [
+                "pytest>=6.0",
+                "pytest-cov>=2.0",
+                "mypy>=0.910",
+                "flake8>=3.9",
+                "tox>=3.24",
+            ],
+        },
+    )
