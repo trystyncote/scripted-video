808ac4feb8ada245db5211745097e5bcdadc3ef3
TNTMaster370 <trystynmcote@gmail.com>
2023-04-18 10:27:10 -0600

    Updated the tests to account for the 'allow_empty_lines' flag, which is False by default.

--- tests/test_Scripter.py
+++ tests/test_Scripter.py
@@ -14,8 +14,8 @@ def script_output(file_name):
 
 def test_iteration():
     # Tests for the iteration of the script.
-    assert script_output("example_script_file_1.txt") == "{aAaA;}{bBbB;}{cCcC;}{dDdD}{eEeE;}{}{fFfF;}{gGgG; /*}" \
-           + "{Commented out?}{*/}{}{hHhH}{i;}"
+    assert script_output("example_script_file_1.txt") == "{aAaA;}{bBbB;}{cCcC;}{dDdD}{eEeE;}{fFfF;}{gGgG; /*}" \
+           + "{Commented out?}{*/}{hHhH}{i;}"
 
 
 def script_output_enumerate(file_name):
@@ -31,7 +31,7 @@ def test_iteration_enumerate():
     # Tests for the iteration of the script, where the enumerate function is
     # used for the Scripter class.
     assert script_output_enumerate("example_script_file_1.txt") == "{0, aAaA;}{1, bBbB;}{2, cCcC;}{3, dDdD}{4, eEeE;}" \
-           + "{5, }{6, fFfF;}{7, gGgG; /*}{8, Commented out?}{9, */}{10, }{11, hHhH}{12, i;}"
+           + "{5, fFfF;}{6, gGgG; /*}{7, Commented out?}{8, */}{9, hHhH}{10, i;}"
 
 
 def script_output_comment_clearing(file_name):
@@ -49,8 +49,8 @@ def script_output_comment_clearing(file_name):
 def test_comment_clearing():
     # Tests for whether the script is correctly clearing comments when it's
     # called to.
-    assert script_output_comment_clearing("example_script_file_1.txt") == "{aAaA;}{bBbB;}{cCcC;}{dDdD}{eEeE;}{}" \
-           + "{fFfF;}{gGgG; }{}{}{}{hHhH}{i;}"
+    assert script_output_comment_clearing("example_script_file_1.txt") == "{aAaA;}{bBbB;}{cCcC;}{dDdD}{eEeE;}" \
+           + "{fFfF;}{gGgG; }{}{}{hHhH}{i;}"
 
 
 def script_output_end_line_detection(file_name):
@@ -95,5 +95,5 @@ def test_repeating_iterating_true():
     # Tests for the behaviour if the script is being read twice, when it *is*
     # allowed to.
     assert script_output_repeated_iteration("example_script_file_1.txt", True) == "{aAaA;}{bBbB;}{cCcC;}{dDdD}{eEeE;}" \
-           + "{}{fFfF;}{gGgG; /*}{Commented out?}{*/}{}{hHhH}{i;}{aAaA;}{bBbB;}{cCcC;}{dDdD}{eEeE;}{}{fFfF;}" \
-           + "{gGgG; /*}{Commented out?}{*/}{}{hHhH}{i;}"
+           + "{fFfF;}{gGgG; /*}{Commented out?}{*/}{hHhH}{i;}{aAaA;}{bBbB;}{cCcC;}{dDdD}{eEeE;}{fFfF;}" \
+           + "{gGgG; /*}{Commented out?}{*/}{hHhH}{i;}"
