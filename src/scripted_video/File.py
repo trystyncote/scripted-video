0b343de65fbcd4a50358631f3efd8bc022af076d
TNTMaster370 <trystynmcote@gmail.com>
2023-04-28 13:53:56 -0600

    Changed `find_path_of_file` to work with pathlib.Path objects instead of raw strings.

--- src/scripted_video/File.py
+++ src/scripted_video/File.py
@@ -1,25 +1,31 @@
-import os
-import string
+from pathlib import Path
 import random
+import string
 
 
-def find_file(start_dir, filename):
-    for root, dirs, files in os.walk(start_dir):
-        if filename in files:
-            return os.path.join(root, filename)
+def _recurse_search_child_directory(desired_file_name: str, current_directory: Path):
+    try:
+        for child_path in current_directory.iterdir():
+            if child_path.is_dir():
+                return_context = _recurse_search_child_directory(desired_file_name, child_path)
+                if return_context:
+                    return return_context
+            elif child_path.is_file() and child_path.name == desired_file_name:
+                return child_path
+    except PermissionError:
+        pass
     return None
 
 
-def find_path_of_file(filename):
-    current_dir = os.path.abspath('.')
-    while True:
-        file_path = find_file(current_dir, filename)
-        if file_path:
-            return file_path
-        parent_dir = os.path.dirname(current_dir)
-        if parent_dir == current_dir:
-            raise FileNotFoundError
-        current_dir = parent_dir
+def find_path_of_file(desired_file_name: str) -> Path:
+    complete_directory = Path(".").absolute()
+    current_directory = complete_directory
+
+    while (current_directory := current_directory.parent) != complete_directory.parents[-1]:
+        return_context = _recurse_search_child_directory(desired_file_name, current_directory)
+        if return_context:
+            return return_context
+    raise FileNotFoundError
 
 
 def create_encoder():
