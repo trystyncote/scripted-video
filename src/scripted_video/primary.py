f9a62292064b1f12ac01b0481db768767db76f06
TNTMaster370 <trystynmcote@gmail.com>
2023-05-02 16:45:26 -0600

    Updated structure to call variables.

--- src/scripted_video/primary.py
+++ src/scripted_video/primary.py
@@ -4,38 +4,30 @@ from scripted_video.Compiler import define_prefix
 from scripted_video.Timetable import create_timetable
 from scripted_video.FrameDraw import create_video
 
+from scripted_video.variables.ScriptVariables import ScriptVariables
+
 import logging
 from pathlib import Path
 
 
 def primary():
-    script_variables = {
-        "_HEAD": {
-            "_script_name":  "",
-            "file_name":     "",
-            "frame_rate":    0,
-            "window_height": 0,
-            "window_width":  0
-        },
-        "ADDRESS": {},
-        "BOOL":    {},
-        "FLOAT":   {},
-        "INT":     {},
-        "STRING":  {}
-    }
+    script_variables = ScriptVariables()
+
+    logging.basicConfig(format=">> %(message)s")
+    log_master = logging.getLogger("")
+    log_master.warning("Starting looking for the script file.")
 
     script_file = find_path_of_file("scriptedVideo_demoScript_1.txt")
     script = Scripter(Path(script_file))
-    script_variables["_HEAD"]["_script_name"] = script_file
+    script_variables.metadata.script_name = script_file
 
     timetable_information = []
     object_information = {}
     encoder = create_encoder()
 
-    logging.basicConfig(format=">> %(message)s")
-    log_master = logging.getLogger("")
-
+    log_master.warning("Finished looking for the script file.")
     log_master.warning("Started reading script.")
+
     for _ in script:
         script.clear_comments()
         script.find_line_end()
@@ -44,9 +36,6 @@ def primary():
             continue
 
         hold_value, classification = define_prefix(script.current_line, script_variables)
-
-        if classification == 1:
-            script_variables = hold_value
 
         if classification == 2:
             timetable_information.append(hold_value)
@@ -57,14 +46,12 @@ def primary():
     log_master.warning("Finished reading script.")
     log_master.warning("Started creating the timetable.")
 
-    sorted_timetable, object_information = create_timetable(
-        timetable_information, script_variables
-    )
+    sorted_timetable, object_information = create_timetable(timetable_information)
 
     log_master.warning("Finished creating the timetable.")
     log_master.warning("Started drawing the frames for the video.")
 
-    create_video(sorted_timetable, object_information, encoder, log_master, **script_variables)
+    create_video(sorted_timetable, object_information, encoder, log_master, script_variables)
 
 
 if __name__ == "__main__":
