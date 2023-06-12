b8f1f8ba10601b3a82b08747da5ea088657eeb2b
TNTMaster370 <trystynmcote@gmail.com>
2023-06-12 01:34:12 -0600

    Created a root node for all CST nodes (#21).

---
+++ src/scripted_video/syntax/root_node.py
@@ -0,0 +1,31 @@
+from abc import abstractmethod
+from collections.abc import MutableSequence
+import re
+from typing import Self
+
+
+class SVST_RootNode:
+    __slots__ = ()  # Defining __slots__ prevents creating of __dict__. It's
+    # naturally inherited by subclasses, which is not desired.
+
+    syntax_list: MutableSequence = []
+
+    def __init_subclass__(cls):
+        super().__init_subclass__()
+        if hasattr(cls, "_syntax"):
+            SVST_RootNode.syntax_list.append((cls, cls._syntax))
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}()"
+
+    def __str__(self) -> str:
+        return self.convert_to_string()
+
+    @abstractmethod
+    def convert_to_string(self, *, indent: int = 0, _previous_indent: int = 0) -> str:
+        return NotImplemented
+
+    @classmethod
+    @abstractmethod
+    def evaluate_syntax(cls, match_object: re.Match) -> Self:
+        return NotImplemented
