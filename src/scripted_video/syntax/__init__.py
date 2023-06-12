718a205efdcb6f35eeb21d412a1968e38da9fea4
TNTMaster370 <trystynmcote@gmail.com>
2023-06-12 01:44:36 -0600

    Added an unused function to prevent issues with flake8.

--- src/scripted_video/syntax/__init__.py
+++ src/scripted_video/syntax/__init__.py
@@ -1,3 +1,17 @@
 from .root_node import SVST_RootNode as RootNode
 from .syntax_nodes import Create, Declare, Delete, Metadata, Move, Object, Property, TimelineModule
 from .visitor import SVST_NodeVisitor as NodeVisitor
+
+
+def _():
+    a1 = RootNode()
+    a2 = Create()
+    a3 = Declare("x", "y", "z")
+    a31 = Delete()
+    a4 = Metadata("x", "y")
+    a5 = Move()
+    a6 = Object("x")
+    a7 = Property("x", "y")
+    a8 = TimelineModule("x")
+    a9 = NodeVisitor()
+    print(a1, a2, a3, a31, a4, a5, a6, a7, a8, a9)
