e845d0f68d50cbd14c3e670cf5791e4114db5394
TNTMaster370 <trystynmcote@gmail.com>
2023-06-14 20:48:56 -0600

    Changed the unused function to a __all__ attribute.

--- src/scripted_video/syntax/__init__.py
+++ src/scripted_video/syntax/__init__.py
@@ -3,15 +3,5 @@ from .syntax_nodes import Create, Declare, Delete, Metadata, Move, Object, Prope
 from .visitor import SVST_NodeVisitor as NodeVisitor
 
 
-def _():
-    a1 = RootNode()
-    a2 = Create()
-    a3 = Declare("x", "y", "z")
-    a31 = Delete()
-    a4 = Metadata("x", "y")
-    a5 = Move()
-    a6 = Object("x")
-    a7 = Property("x", "y")
-    a8 = TimelineModule("x")
-    a9 = NodeVisitor()
-    print(a1, a2, a3, a31, a4, a5, a6, a7, a8, a9)
+__all__ = ["RootNode", "NodeVisitor", "Create", "Declare", "Delete", "Metadata", "Move", "Object", "Property",
+           "TimelineModule"]
