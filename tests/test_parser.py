06a06a92357bb6ad614c332b4f1e62bf51582696
TNTMaster370 <trystynmcote@gmail.com>
2023-06-16 23:04:46 -0600

    Added use of 'find_path_of_file' in an attempt to fix a FileNotFoundError.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,3 +1,4 @@
+from src.scripted_video.File import find_path_of_file
 from src.scripted_video.Scripter import script_parser
 
 
@@ -6,7 +7,7 @@ def test_parser_default():
     idx = 0
     expected = ["aAaA;", "bBbB;", "cCcC; // Commented out?", "dDdD", "eEeE;", "fFfF;", "gGgG; /*", "Commented out?",
                 "*/", "hHhH", "i;"]
-    for actual in script_parser("example_script_file_1.txt"):
+    for actual in script_parser(find_path_of_file("example_script_file_1.txt")):
         assert actual == expected[idx]
         idx += 1
 
@@ -16,7 +17,7 @@ def test_parser_inline_comments():
     idx = 0
     expected = ["aAaA;", "bBbB;", "cCcC;", "dDdD", "eEeE;", "fFfF;", "gGgG; /*", "Commented out?",
                 "*/", "hHhH", "i;"]
-    for actual in script_parser("example_script_file_1.txt", inline_comment_character="//"):
+    for actual in script_parser(find_path_of_file("example_script_file_1.txt"), inline_comment_character="//"):
         assert actual == expected[idx]
         idx += 1
 
@@ -25,7 +26,7 @@ def test_parser_block_comments():
     """ Tests the ability to specify a set of affixes to block comments. """
     idx = 0
     expected = ["aAaA;", "bBbB;", "cCcC; // Commented out?", "dDdD", "eEeE;", "fFfF;", "gGgG;", "hHhH", "i;"]
-    for actual in script_parser("example_script_file_1.txt", block_comment_characters=("/*", "*/")):
+    for actual in script_parser(find_path_of_file("example_script_file_1.txt"), block_comment_characters=("/*", "*/")):
         assert actual == expected[idx]
         idx += 1
 
@@ -34,7 +35,7 @@ def test_parser_comments_both_types():
     """ Tests the compatibility of comments system when both are specified. """
     idx = 0
     expected = ["aAaA;", "bBbB;", "cCcC;", "dDdD", "eEeE;", "fFfF;", "gGgG;", "hHhH", "i;"]
-    for actual in script_parser("example_script_file_1.txt", block_comment_characters=("/*", "*/"),
+    for actual in script_parser(find_path_of_file("example_script_file_1.txt"), block_comment_characters=("/*", "*/"),
                                 inline_comment_character="//"):
         assert actual == expected[idx]
         idx += 1
@@ -45,7 +46,7 @@ def test_parser_end_line():
         character that concludes a line.       """
     idx = 0
     expected = ["aAaA", "bBbB", "cCcC", "dDdD eEeE", "fFfF", "gGgG", "Commented out? */ hHhH i"]
-    for actual in script_parser("example_script_file_1.txt", end_line_character=";"):
+    for actual in script_parser(find_path_of_file("example_script_file_1.txt"), end_line_character=";"):
         assert actual == expected[idx]
         idx += 1
 
@@ -55,7 +56,7 @@ def test_parser_end_line_with_comments():
         character with both types of comments. """
     idx = 0
     expected = ["aAaA", "bBbB", "cCcC", "dDdD eEeE", "fFfF", "gGgG", "hHhH i"]
-    for actual in script_parser("example_script_file_1.txt", end_line_character=";",
+    for actual in script_parser(find_path_of_file("example_script_file_1.txt"), end_line_character=";",
                                 block_comment_characters=("/*", "*/"), inline_comment_character="//"):
         assert actual == expected[idx]
         idx += 1
