21a3edf483807fe30b722be74d748ec0176d5603
TNTMaster370 <trystynmcote@gmail.com>
2023-06-18 20:01:24 -0600

    Added Doctype to __init__.py

--- src/scripted_video/syntax/__init__.py
+++ src/scripted_video/syntax/__init__.py
@@ -1,7 +1,7 @@
 from .root_node import SVST_RootNode as RootNode
-from .syntax_nodes import Create, Declare, Delete, Metadata, Move, Object, Property, TimelineModule
+from .syntax_nodes import Create, Declare, Delete, Doctype, Metadata, Move, Object, Property, TimelineModule
 from .visitor import SVST_NodeVisitor as NodeVisitor
 
 
-__all__ = ["RootNode", "NodeVisitor", "Create", "Declare", "Delete", "Metadata", "Move", "Object", "Property",
+__all__ = ["RootNode", "NodeVisitor", "Create", "Declare", "Delete", "Doctype", "Metadata", "Move", "Object", "Property",
            "TimelineModule"]
