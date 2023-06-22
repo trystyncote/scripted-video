a82a86b3bd585cae2f0ca3e1c124890bcdd5b51d
TNTMaster370 <trystynmcote@gmail.com>
2023-06-22 15:22:50 -0600

    Fixed a bug where syntax_list.items() wasn't being called.

--- src/scripted_video/compile_time.py
+++ src/scripted_video/compile_time.py
@@ -10,7 +10,7 @@ def create_syntax_tree_root(script_name: str) -> svSyntax.TimelineModule:
 
 
 def dissect_syntax(command: str, syntax_tree):
-    for respective_class, syntax_command in svSyntax.RootNode.syntax_list:
+    for respective_class, syntax_command in svSyntax.RootNode.syntax_list.items():
         match = re.match(syntax_command, command)
         if not match:
             continue
