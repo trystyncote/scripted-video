2c9891d591fffb188d5f3bccbe91414a0b78d173
TNTMaster370 <trystynmcote@gmail.com>
2023-06-23 17:17:09 -0600

    Extracted part of MoveInstruction functionality into a root class.

--- src/scripted_video/objects/instruction.py
+++ src/scripted_video/objects/instruction.py
@@ -1,36 +1,21 @@
 from src.scripted_video.objects._time import TIME, _manage_time
 
 
-class MoveInstruction:
-    __slots__ = ("_bound_object", "_frame_rate", "rate", "scale", "time", "x", "y")
+class RootInstruction:
+    __slots__ = ("_bound_object", "_frame_rate")
 
     _bound_object: str
     _frame_rate: int
-    rate: TIME
-    scale: float
-    time: TIME
-    x: int
-    y: int
 
     def __init__(self, bound_object: str, frame_rate: int):
         self._bound_object = bound_object
         self._frame_rate = frame_rate
 
-        self.scale = 0.0
-        self.x = 0
-        self.y = 0
-
     @property
     def bound_object(self):
         return self._bound_object
 
-    def evaluate_attributes(self) -> bool:
-        for attr in self.__slots__:
-            if getattr(self, attr) is None:
-                return False
-        return True
-
-    def set_attribute(self, name: str, value):
+    def set_attribute(self, name: str, value: str):
         value = self.validate_attribute(name, value)
         self.__setattr__(name, value)
 
@@ -41,3 +26,26 @@ class MoveInstruction:
         else:
             value = annotation(value)
         return value
+
+    def evaluate_attributes(self) -> bool:
+        for attr in self.__slots__:
+            if getattr(self, attr, None) is None:
+                return False
+        return True
+
+
+class MoveInstruction(RootInstruction):
+    __slots__ = ("rate", "scale", "time", "x", "y")
+
+    rate: TIME
+    scale: float
+    time: TIME
+    x: int
+    y: int
+
+    def __init__(self, bound_object: str, frame_rate: int):
+        super().__init__(bound_object, frame_rate)
+
+        self.scale = 0.0
+        self.x = 0
+        self.y = 0
