2400da3174aedaef0691c347c07228067ef83989
TNTMaster370 <trystynmcote@gmail.com>
2023-06-23 13:00:23 -0600

    Updated the expected result to the match_ functions to a class.

--- tests/_syntax_match_functions.py
+++ tests/_syntax_match_functions.py
@@ -1,25 +1,39 @@
 import src.scripted_video.syntax as syntax
 
 
-def match_Declare(node):
+def match_Create(node, exp):
     match node:
-        case syntax.Declare(name="abc", value="def", type="ghi"):
+        case syntax.Create(
+            body=[
+                syntax.Property(name=exp.p_name_A, value=exp.p_value_A),
+                syntax.Property(name=exp.p_name_B, value=exp.p_value_B)
+            ],
+            subjects=[syntax.Object(name=exp.o_name)]
+        ):
             assert True
         case _:
             assert False
 
 
-def match_Doctype(node):
+def match_Declare(node, exp):
     match node:
-        case syntax.Doctype(doctype="scripted-video"):
+        case syntax.Declare(name=exp.name, value=exp.value, type=exp.type):
             assert True
         case _:
             assert False
 
 
-def match_Metadata(node):
+def match_Doctype(node, exp):
     match node:
-        case syntax.Metadata(name="window_width", value="852"):
+        case syntax.Doctype(doctype=exp.doc):
+            assert True
+        case _:
+            assert False
+
+
+def match_Metadata(node, exp):
+    match node:
+        case syntax.Metadata(name=exp.name, value=exp.value):
             assert True
         case _:
             assert False
--- tests/test_syntax.py
+++ tests/test_syntax.py
@@ -1,21 +1,37 @@
-from tests._syntax_match_functions import match_Declare, match_Doctype, match_Metadata
+from tests._syntax_match_functions import match_Create, match_Declare, match_Doctype, match_Metadata
 
 from src.scripted_video.syntax.root_node import SVST_RootNode as RootNode
-from src.scripted_video.syntax.syntax_nodes import Declare, Doctype, Metadata
+from src.scripted_video.syntax.syntax_nodes import Create, Declare, Doctype, Metadata
 
 import pytest
 import re
 
 
-@pytest.mark.parametrize("command,cls,match_callable", [
-    ("@DOCTYPE scripted-video", Doctype, match_Doctype),
-    ("HEAD window_width = 852", Metadata, match_Metadata),
-    ("SET abc = def AS ghi", Declare, match_Declare)
+class Expected:
+    __slots__ = ("__dict__",)  # preventing creation of __weakref__.
+
+    def set(self, name, value):
+        """ Wrapper for setting an attribute. """
+        self.__setattr__(name, value)  # this is to allow setting an attribute
+        # from a string.
+
+
+@pytest.mark.parametrize("command,cls,match_callable,expected_attr", [
+    ("@DOCTYPE scripted-video", Doctype, match_Doctype, "doc:scripted-video"),
+    ("HEAD window_width = 852", Metadata, match_Metadata, "name:window_width;value:852"),
+    ("SET abc = def AS ghi", Declare, match_Declare, "name:abc;value:def;type:ghi"),
+    ("CREATE *obj { x: 100; y: 100; }", Create, match_Create,
+     "o_name:*obj;p_name_A:x;p_value_A:100;p_name_B:y;p_value_B:100")
 ])
-def test_syntax_evaluate(command, cls, match_callable):
+def test_syntax_evaluate(command, cls, match_callable, expected_attr):
+    expected = Expected()
+    for attr in expected_attr.split(";"):
+        expected.set(*attr.split(":"))  # assume that the result will be of
+        # length 2.
+
     query = RootNode.syntax_list[cls]
     match = re.match(query, command)
     if match is None:
         assert False
     node = cls.evaluate_syntax(match)
-    match_callable(node)
+    match_callable(node, expected)
