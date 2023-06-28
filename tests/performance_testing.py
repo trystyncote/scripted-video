1d833946a6610b2d66abdd22fc43723fde3b9037
TNTMaster370 <trystynmcote@gmail.com>
2023-06-27 22:48:11 -0600

    Fixed all issues with detect_performance().

--- tests/performance_testing.py
+++ tests/performance_testing.py
@@ -1,8 +1,7 @@
-from src.scripted_video.utils import find_path_of_file
+from src.scripted_video.utils import find_path_of_file, Options
 from src.scripted_video.primary import generate_script
 
 import cProfile
-import logging
 from pathlib import Path
 import pstats
 
@@ -11,12 +10,16 @@ def detect_performance():
     # Code borrowed from mCoding. See the following link:
     # https://github.com/mCodingLLC/VideosSampleCode/blob/master/videos/063_find_why_your_python_code_is_slow_using_this_essential_tool_dot___feat_dot__async_await_/needs_profiling.py
     script = Path(find_path_of_file("svDemo1.txt"))
-    logging.basicConfig(format=">> %(message)s")
-    logger = logging.getLogger("")
+    options = Options()
+    options.verbose = False
 
     with cProfile.Profile() as pr:
-        generate_script(script, logger)
+        generate_script(script, options)
 
     stats = pstats.Stats(pr)
     stats.sort_stats(pstats.SortKey.TIME)
     stats.print_stats()
+
+
+if __name__ == "__main__":
+    detect_performance()
