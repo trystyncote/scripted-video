08351546bf43330cc8257afe9a5515e256ea4dac
TNTMaster370 <trystynmcote@gmail.com>
2023-06-28 23:06:53 -0600

    Fixed options.extras_require configuration.

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
 from setuptools import setup  # , find_packages
 
 if __name__ == "__main__":
-    setup(
-        extras_require={
-            "testing": [
-                "pytest>=6.0",
-                "pytest-cov>=2.0",
-                "mypy>=0.910",
-                "flake8>=3.9",
-                "tox>=3.24",
-            ],
-        },
-    )
+    setup()
