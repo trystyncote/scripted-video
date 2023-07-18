5f8603f57c313add1e7cc2efef37345a26d4697e
TNTMaster370 <trystynmcote@gmail.com>
2023-07-18 13:06:20 -0600

    Stripped all methods out of SVST_RootNode.

--- scripted_video/svst/root_node.py
+++ scripted_video/svst/root_node.py
@@ -1,26 +1,3 @@
-import re
-from typing import Self
-
-
 class SVST_RootNode:
     __slots__ = ()  # Defining __slots__ prevents creating of __dict__. It's
     # naturally inherited by subclasses, which is not desired.
-
-    def __init__(self, *args, **kwargs):
-        if args:
-            raise ValueError(f"{self.__class__.__name__}.__init__ has excess *args: {args}")
-        elif kwargs:
-            raise ValueError(f"{self.__class__.__name__}.__init__ has excess *kwargs: {kwargs}")
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}()"
-
-    def __str__(self) -> str:
-        return self.convert_to_string()
-
-    def convert_to_string(self, *, indent: int = 0, _previous_indent: int = 0) -> str:
-        return NotImplemented
-
-    @classmethod
-    def evaluate_syntax(cls, match_object: re.Match) -> Self:
-        return NotImplemented
