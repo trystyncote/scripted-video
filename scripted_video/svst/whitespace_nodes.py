4744fd1d17f6c8ac00c47cbded18d13ca9e27e35
TNTMaster370 <trystynmcote@gmail.com>
2023-07-17 22:32:48 -0600

    Removed redundant __slots__ from SimpleWhitespace.

--- scripted_video/svst/whitespace_nodes.py
+++ scripted_video/svst/whitespace_nodes.py
@@ -5,7 +5,6 @@ from .root_node import SVST_RootNode
 @dynamic_attributes
 class SimpleWhitespace(SVST_RootNode):
     __attributes__ = (Attribute.VALUE,)
-    __slots__ = ()  # preventing creation of __dict__.
 
     @property
     def empty(self):
