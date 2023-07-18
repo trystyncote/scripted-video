e16f89425223c9f94bf2dbff9ce267892261c024
TNTMaster370 <trystynmcote@gmail.com>
2023-07-18 12:58:15 -0600

    Added a NeutralNode superclass to all neutral nodes.

--- scripted_video/svst/neutral_nodes.py
+++ scripted_video/svst/neutral_nodes.py
@@ -1,12 +1,24 @@
 from ._dynamic_attributes import Attribute, dynamic_attributes, SpecificAttribute
 from .root_node import SVST_RootNode
 
+from collections.abc import MutableMapping
 import re
 from typing import Self
 
 
+class NeutralNode(SVST_RootNode):
+    __slots__ = ()
+
+    syntax_list: MutableMapping = {}
+
+    def __init_subclass__(cls):
+        super().__init_subclass__()
+        if hasattr(cls, "_syntax"):
+            NeutralNode.syntax_list[cls.__name__] = (cls, cls._syntax)
+
+
 @dynamic_attributes
-class Doctype(SVST_RootNode):
+class Doctype(NeutralNode):
     __attributes__ = (SpecificAttribute.DOCTYPE,)
     syntax = r"@DOCTYPE [\s| ]*(scripted-video){1}"
     # syntax = r"@DOCTYPE [\s| ]*(scripted-video){1}[\s| ]+((?:TIMELINE)|(?:MASTER[-]*SCRIPT)|(?:DESIGN)){1};"
@@ -19,7 +31,7 @@ class Doctype(SVST_RootNode):
 
 
 @dynamic_attributes
-class Object(SVST_RootNode):
+class Object(NeutralNode):
     """
     The Object node refers to a reference to an object, via the approximate
     syntax [*<object>]. The name of the object is referenced as the 'name'.
@@ -29,7 +41,7 @@ class Object(SVST_RootNode):
 
 
 @dynamic_attributes
-class Property(SVST_RootNode):
+class Property(NeutralNode):
     """
     The Property node refers to a call to a property and its value. The name of
     the property is referenced as the 'name', and its value as 'value'. Meant
@@ -39,5 +51,5 @@ class Property(SVST_RootNode):
 
 
 @dynamic_attributes
-class UnknownSyntax(SVST_RootNode):
+class UnknownSyntax(NeutralNode):
     __attributes__ = (SpecificAttribute.CONTENTS,)
