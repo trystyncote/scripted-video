4cef73c7c95c1bd6bce0cf072f67dda188173085
TNTMaster370 <trystynmcote@gmail.com>
2023-07-22 14:18:51 -0600

    Added a svst.dump() function that causes the printout.

--- scripted_video/svst/__init__.py
+++ scripted_video/svst/__init__.py
@@ -1,7 +1,8 @@
+from .dump import dump
 from .neutral_nodes import Doctype, NeutralNode, Object, Property, UnknownSyntax
 from .root_node import SVST_RootNode as RootNode
 from .timeline_nodes import Create, Declare, Delete, Metadata, Move, TimelineModule, TimelineNode
 
 
-__all__ = ["Create", "Declare", "Delete", "Doctype", "Metadata", "Move", "NeutralNode", "Object", "Property",
+__all__ = ["Create", "Declare", "Delete", "Doctype", "dump", "Metadata", "Move", "NeutralNode", "Object", "Property",
            "RootNode", "TimelineModule", "TimelineNode", "UnknownSyntax"]
